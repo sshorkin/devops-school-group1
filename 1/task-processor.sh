70425ec0723d335e6ee9cf0e267ae7cac571318c
Sergii Shorkin <aa.bootis@gmail.com>
2024-02-17 18:55:03 +0200

    [TASK-1] add readme

--- 1/task-processor.sh
+++ 1/task-processor.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-#
-# Write a Bash script that processes tasks from the JSON file.
-# Include a check for the presence of the JSON file in the script.
-# If the JSON file is missing, output an appropriate message.
-# If the file is absent, display an error message and terminate the script execution.
-# Retrieve the current date in a format consistent with the dates in the JSON file.
-# Use a loop to iterate through the tasks in the JSON file.
-# For each task, check if its deadline has passed by comparing the deadline date with the current date.
-# If a task has a passed deadline, output information about it (id, title, description, status, deadline).
-# Use a flag to track whether overdue tasks were found during the script execution.
-# If no overdue tasks are found, output a message indicating the absence of overdue tasks.
-#
 
 export CURRENT_DATE=$(date +"%Y-%m-%d")
 
